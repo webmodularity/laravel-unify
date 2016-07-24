177ea676aa82e31419d3e91d0f3aa4bc58c6b1c3
Rory Hellyer <rory@webmodularity.com>
2016-07-23 18:55:05 -0700

    Initial Commit

---
+++ src/UnifyServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WebModularity\LaravelUnify;
+
+use Illuminate\Support\ServiceProvider;
+
+class UnifyServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        // Views
+        $viewPath = __DIR__ . '../resources/views';
+        $this->loadViewsFrom($viewPath, 'unify');
+        $this->publishes([$viewPath => base_path('resources/views/vendor/unify')], 'views');
+
+        // Config
+        $configPath = __DIR__ . '../config/unify.php';
+        $this->publishes([$configPath => config_path('unify.php')], 'config');
+
+        // Assets
+        $this->publishes(['../resources/assets' => public_path('vendor/unify')], 'assets');
+    }
+}
