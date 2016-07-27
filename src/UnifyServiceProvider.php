1a9928f434e17f153b358828a57e5e9850e0d995
Rory Hellyer <rory@webmodularity.com>
2016-07-26 22:09:20 -0700

    fix missing / in publish path

--- src/UnifyServiceProvider.php
+++ src/UnifyServiceProvider.php
@@ -13,15 +13,15 @@ class UnifyServiceProvider extends ServiceProvider
 
     public function boot() {
         // Views
-        $viewPath = __DIR__ . 'resources/views';
+        $viewPath = __DIR__ . '/resources/views';
         $this->loadViewsFrom($viewPath, 'unify');
         $this->publishes([$viewPath => base_path('resources/views/vendor/unify')], 'views');
 
         // Config
-        $configPath = __DIR__ . 'config/unify.php';
+        $configPath = __DIR__ . '/config/unify.php';
         $this->publishes([$configPath => config_path('unify.php')], 'config');
 
         // Assets
-        $this->publishes([__DIR__ . 'resources/assets' => public_path('vendor/unify')], 'assets');
+        $this->publishes([__DIR__ . '/resources/assets' => public_path('vendor/unify')], 'assets');
     }
 }
