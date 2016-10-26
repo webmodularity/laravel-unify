19778d43cdd1ab9a4403ed0da58cd4906b84b984
Rory Hellyer <rory@webmodularity.com>
2016-10-25 21:48:42 -0700

    Remove features from config

--- src/config/unify.php
+++ src/config/unify.php
@@ -7,13 +7,5 @@ return [
     'header_version_style' => null,
     'footer_version' => null,
     'theme_color' => null,
-    'theme_dark' => false,
-    'features' => [
-        'cube-portfolio' => false,
-        'fancybox' => false,
-        'owl-carousel' => false,
-        'parallax' => false,
-        'parallax-slider' => false,
-        'wow-animations' => false
-    ]
+    'theme_dark' => false
 ];
