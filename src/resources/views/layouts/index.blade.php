6a1c704132cf9d6463d2442705a6dd9bbecca95b
Rory Hellyer <rory@webmodularity.com>
2016-10-25 21:54:04 -0700

    jquery-ready

--- src/resources/views/layouts/index.blade.php
+++ src/resources/views/layouts/index.blade.php
@@ -14,27 +14,4 @@ Default Page
 
 @push('jquery-ready')
 App.init();
-
-//Owl Clients v1
-jQuery(".owl-clients-v1").owlCarousel({
-items : 7,
-autoPlay : 5000,
-itemsDesktop : [1000,5],
-itemsDesktopSmall : [900,4],
-itemsTablet: [600,3],
-itemsMobile : [300,2]
-});
-
-$('#da-slider').cslider({
-current     : 0,
-// index of current slide
-
-bgincrement : 50,
-// increment the background position
-// (parallax effect) when sliding
-autoplay    : false,
-// slideshow on / off
-interval    : 4000
-// time between transitions
-});
 @endpush
